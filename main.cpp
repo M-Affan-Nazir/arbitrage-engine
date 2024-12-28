48776431bf6f735764a70ae54381f80e9fa70dc3
M-Affan-Nazir <m.affann80@hotmail.com>
2024-12-28 13:55:23 -0700

    Currency -log(Exchange Rate) Graph Completed

--- main.cpp
+++ main.cpp
@@ -1,12 +1,53 @@
 #include <currencyPairs.hpp>
 #include <GraphGenerator.hpp>
+#include <cmath>
 #include <stdio.h>
+using namespace std;
 
-int main() {
-    
+Graph getFilledGraph(){
+
+    //Initialization
     CurrencyPairs currencies = CurrencyPairs("82f6fed8a66de96e7f6eee2c");
     currencies.fetch();
     Graph graph = Graph(currencies.get_unique_keys().size());
     graph.set_key(currencies.get_unique_keys());
+    
+    //Filling:
+    vector<string> usdKey = currencies.getUsdKey();
+    vector<float> usdEdge = currencies.getUsdEdge();
+
+    vector<string> eurKey = currencies.getEurKey();
+    vector<float> eurEdge = currencies.getEurEdge();
+
+    vector<string> jpyKey = currencies.getJpyKey();
+    vector<float> jpyEdge = currencies.getJpyEdge();
+
+    vector<string> gbpKey = currencies.getGbpKey();
+    vector<float> gbpEdge = currencies.getGbpEdge();
+
+    vector<string> chfKey = currencies.getChfKey();
+    vector<float> chfEdge = currencies.getChfEdge();
+
+    for(int i = 0; i<usdKey.size();i++){
+        graph.add_edge_using_key("USD", usdKey[i], -log(usdEdge[i]));
+    }
+    for(int i = 0; i<eurKey.size();i++){
+        graph.add_edge_using_key("EUR", eurKey[i], -log(eurEdge[i]));
+    }
+    for(int i = 0; i<jpyKey.size();i++){
+        graph.add_edge_using_key("JPY", jpyKey[i], -log(jpyEdge[i]));
+    }
+    for(int i = 0; i<gbpKey.size();i++){
+        graph.add_edge_using_key("GBP", gbpKey[i], -log(gbpEdge[i]));
+    }
+    for(int i = 0; i<chfKey.size();i++){
+        graph.add_edge_using_key("CHF", chfKey[i], -log(chfEdge[i]));
+    }
+
+    return graph;
+}
+
+int main() {
+    Graph graph = getFilledGraph();
     graph.print_matrix();
 }

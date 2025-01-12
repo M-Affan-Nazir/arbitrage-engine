d668fed9c6c3126a2d6049c34c6e346baf7abb10
M-Affan-Nazir <m.affann80@hotmail.com>
2025-01-12 13:31:48 -0700

    Finished MinPriorityQueue implementation

--- include/minPriorityQueue.hpp
+++ include/minPriorityQueue.hpp
@@ -2,3 +2,28 @@
 #include <functional>
 #include <string>
 using namespace std;
+
+struct Node{
+
+    int index;
+    int predIndex;
+    float distance;
+    Node(int idx, int predIdx, float dist) : index(idx), predIndex(predIdx), distance(dist) {} //constructor
+
+};
+
+class MinPQ{
+
+    private:
+        struct compareDistance{
+            bool operator()(Node &a, Node &b){
+                return a.distance > b.distance;
+            }  // allowing struct to be called as a function, overloading '()' operation. compareDistance comp(a,b) -> boolean
+        };
+        priority_queue<Node, vector<Node>, compareDistance> minPQ;
+    public:
+        MinPQ();
+        void addNode(int index, int predIndex, float distance);
+        Node extractMinAndPop();
+        void adjustNode(int index, float newDistance);
+};
--- src/minPriorityQueue.cpp
+++ src/minPriorityQueue.cpp
@@ -1,4 +1,34 @@
 #include <queue>
 #include <functional>
 #include <string>
+#include <minPriorityQueue.hpp>
 using namespace std;
+
+MinPQ::MinPQ(){}
+
+void MinPQ::addNode(int index, int predIndex, float distance){
+    this->minPQ.push(Node(index, predIndex, distance));
+}
+
+Node MinPQ::extractMinAndPop(){
+    Node minElement = this->minPQ.top();
+    this->minPQ.pop();
+    return minElement;
+}
+
+void MinPQ::adjustNode(int index, float newDistance){   //to be revised
+    vector<Node> temp;
+
+    while(!minPQ.empty()){
+        Node top = minPQ.top();
+        minPQ.pop();
+
+        if(top.index == index){
+            top.distance = newDistance;
+        }
+        temp.push_back(top);
+    }
+    for(auto &node : temp){
+        minPQ.push(node);
+    }
+}
